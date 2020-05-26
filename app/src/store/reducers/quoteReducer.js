b7810518a642a7090866dc627ed9f4f7805d5117
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-26 14:23:18 -0700

    initial state in store/reducers/quoteReducer.js

---
+++ app/src/store/reducers/quoteReducer.js
@@ -0,0 +1,12 @@
+const intitialState = {
+    quote: '',
+    isFetching: false,
+    error: ''
+}
+
+export const quotesReducer = (state = intitialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
