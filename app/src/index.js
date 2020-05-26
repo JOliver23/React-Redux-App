f89ba2ea2d97d7a570c534db94975802ce3912f4
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-26 14:18:26 -0700

    redcuer folder/file mad

--- app/src/index.js
+++ app/src/index.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
+import { rootReducer } from './store/reducers';
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
